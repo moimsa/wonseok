1b6e39892515d0a4468ec810a20a871834466754
Dseok <ehskstkrjsa@naver.com>
2025-06-18 23:32:19 +0900

    DB 데이터 출력하려면 강의에서 List<>이 부분 오류

---
+++ spring/shop/shop/src/main/java/com/example/shop/Item.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String title;
+    public Integer price;
+
+}
--- spring/shop/shop/src/main/java/com/example/shop/ItemController.java
+++ spring/shop/shop/src/main/java/com/example/shop/ItemController.java
@@ -1,13 +1,27 @@
 package com.example.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     String list(Model model){
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+        System.out.println(result.get(0).title);
+
+//        List<Item> result = itemRepository.findAll();
+//        System.out.println(result.get(0));
+
         model.addAttribute("name", "홍길동");
         return "list.html";
     }
